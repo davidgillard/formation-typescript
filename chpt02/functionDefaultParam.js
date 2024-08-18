3ce6aceea0e1edf310c26f8a1ee509a41c508aba
David GILLARD <ddavid.ggillard@protonmail.com>
2024-08-18 17:41:50 +0200

    function Optional Parameters

---
+++ chpt02/functionDefaultParam.js
@@ -0,0 +1,18 @@
+function concatWithDefault(a, b) {
+    if (b === void 0) { b = "default"; }
+    console.log("a + b = ".concat(a + b));
+}
+concatWithDefault("first", "second");
+concatWithDefault("third");
+// Explication: 
+// Ici, nous avons défini une fonction nommée concatWithDefault qui possède deux paramètres, a et b, tous deux de type chaîne.
+// Notez cependant la définition du paramètre nommé b. Nous attribuons la valeur « default » à ce paramètre dans la définition
+// de la fonction. Cette affectation rendra automatiquement ce paramètre facultatif, et nous n'utilisons pas la syntaxe du point
+// d'interrogation pour définir ce paramètre comme facultatif. Notez également que l'utilisation du type explicite pour le paramètre
+// b, comme dans :string, est également facultative, car le compilateur déduira le type de la valeur par défaut, qui dans ce cas est
+// le type chaîne. Nous appelons ensuite cette fonction avec deux arguments, puis avec un seul argument.
+// Le résultat de ce code est le suivant :
+// a + b = firstsecond
+// a + b = thirddefault
+// Ici, nous pouvons voir que lorsque nous fournissons deux arguments à la fonction concatWithDefault, la fonction concatène les 
+// arguments comme prévu. Lorsque nous ne fournissons qu'un seul argument, le deuxième argument aura par défaut la valeur « default ».
---
+++ chpt02/functionOptionalParam.js
@@ -0,0 +1,16 @@
+function concalValues(a, b) {
+    console.log("a + b = ".concat(a + b));
+}
+concalValues("first", "second");
+concalValues("third");
+// Explication: 
+// Ici, nous pouvons voir que le premier appel à la fonction concatValues concatène les chaînes « first » et « second »,
+// en enregistrant la valeur de « firstsecond » dans la console. Le deuxième appel à la fonction concatValues n'a fourni
+// qu'une valeur pour le premier argument, car le deuxième argument était marqué comme facultatif. Ce deuxième appel à la
+// fonction concatValues produit la sortie « thirdundefined »,car nous n'avons pas spécifié de valeur pour le deuxième argument.
+// Cela signifie que l'argument b n'a pas été spécifié et est donc indéfini.
+// a + b = firstsecond
+// a + b = thirdundefined
+// Notez que tous les paramètres facultatifs doivent être répertoriés en dernier dans la liste des paramètres de la définition
+// de fonction. Vous pouvez avoir autant de paramètres facultatifs que vous le souhaitez, tant que les paramètres non facultatifs
+// précèdent les paramètres facultatifs.

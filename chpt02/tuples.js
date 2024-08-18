1c9e8467a6125b5cefc7bf225fbad56b7fd654dd
David GILLARD <ddavid.ggillard@protonmail.com>
2024-08-18 10:00:34 +0200

    tuples

---
+++ chpt02/tuples.js
@@ -0,0 +1,4 @@
+var tuple1;
+tuple1 = ["test", true];
+tuple1 = ["test", false];
+console.log("tuple1 = ".concat(tuple1));
---
+++ chpt02/tuples.ts
@@ -0,0 +1,7 @@
+let tuple1: [string, boolean];
+tuple1 = ["test", true];
+
+tuple1 = ["test", false];
+
+console.log(`tuple1 = ${tuple1}`);
+

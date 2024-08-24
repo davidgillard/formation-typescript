d488200e9e170d770e0770c9f59235b125fe7db7
David GILLARD <ddavid.ggillard@protonmail.com>
2024-08-24 09:45:53 +0200

    les interfaces

---
+++ chpt03/interfaces.js
@@ -0,0 +1,7 @@
+var idObject = {
+    id: 2,
+    name: "This is a name"
+};
+// Explication:
+// L'interface créer avec le mot clé interface, cette interface décrit un object qui a une propriété de type 
+// number et une propriété de type string. Une fois l'interface créer, elle peut être utilisé comme les types primitifs.

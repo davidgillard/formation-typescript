fc58bd2c3ce9f8347a7f4d4b001fcd79a9f6b0e9
David GILLARD <ddavid.ggillard@protonmail.com>
2024-08-30 15:43:03 +0200

    les génériques

--- chpt04/printGeneric.js
+++ chpt04/printGeneric.js
@@ -7,3 +7,35 @@ printGeneric("test");
 printGeneric(true);
 printGeneric(function () { });
 printGeneric({ id: 1 });
+printGeneric("test");
+// printGeneric<string>(1);
+function usingTwoTypes(first, second) {
+    // console.log(`typeof A, B is : ${typeof A}, ${typeof B}`);
+    // console.log(`value is : ${A}, {B}`)
+}
+usingTwoTypes(1, "test");
+usingTwoTypes(1, "test");
+usingTwoTypes(true, false);
+usingTwoTypes("first", "second");
+var Concatenator = /** @class */ (function () {
+    function Concatenator() {
+    }
+    Concatenator.prototype.concatenateArray = function (items) {
+        var returnString = "";
+        for (var i = 0; i < items.length; i++) {
+            returnString += i > 0 ? "," : "";
+            returnString += items[i].toString();
+        }
+        return returnString;
+    };
+    return Concatenator;
+}());
+var concator = new Concatenator();
+var concateResult = concator.concatenateArray([
+    "первый", "второй", "третий"
+]);
+console.log("concateResult = ".concat(concateResult));
+concateResult = concator.concatenateArray([
+    1234, 5678, 9012
+]);
+console.log("concateResult = ".concat(concateResult));
--- chpt04/printGeneric.ts
+++ chpt04/printGeneric.ts
@@ -8,3 +8,40 @@ printGeneric("test");
 printGeneric(true);
 printGeneric(() => { });
 printGeneric({ id: 1});
+printGeneric<string>("test");
+// printGeneric<string>(1);
+
+function usingTwoTypes<A, B>(first: A, second: B) {
+  // console.log(`typeof A, B is : ${typeof A}, ${typeof B}`);
+  // console.log(`value is : ${A}, {B}`)
+}
+
+usingTwoTypes<number, string>(1, "test");
+usingTwoTypes(1, "test");
+usingTwoTypes<boolean, boolean>(true, false);
+usingTwoTypes("first", "second");
+
+
+class Concatenator<T extends Array<string> | Array<number>> {
+  public concatenateArray(items: T): string {
+    let returnString = "";
+    for (let i = 0; i < items.length; i++) {
+      returnString += i > 0 ? "," : "";
+      returnString += items[i].toString();
+    }
+    return returnString;
+  }
+}
+
+let concator = new Concatenator();
+
+let concateResult = concator.concatenateArray([
+  "первый", "второй", "третий" 
+]);
+console.log(`concateResult = ${concateResult}`);
+
+concateResult = concator.concatenateArray([
+  1234, 5678, 9012
+]);
+console.log(`concateResult = ${concateResult}`);
+

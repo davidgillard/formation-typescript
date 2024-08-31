f21a729595bf4a268f518b54a5ee4ce1b2fe0462
David GILLARD <ddavid.ggillard@protonmail.com>
2024-08-31 14:56:04 +0200

    Distributed conditional types

---
+++ chpt04/DistributedConditionalType.js
@@ -0,0 +1,8 @@
+function compareValues(input, compareTo) {
+    compareValues(new Date(), new Date());
+    compareValues(1, new Date());
+    compareValues(1, 2);
+    compareValues("test", new Date());
+    compareValues("test", 1);
+    compareValues("test", "test");
+}
---
+++ chpt04/DistributedConditionalType.ts
@@ -0,0 +1,18 @@
+type dateOrNumberOrString<T> = T extends Date ? Date : 
+T extends number ? Date | number :
+T extends string ? Date | number | string :
+never;
+
+function compareValues<T extends string | number | Date | boolean>
+  (input : T, 
+  compareTo: dateOrNumberOrString<T>  
+) {
+compareValues(new Date(), new Date());
+compareValues(1, new Date());
+compareValues(1, 2);
+compareValues("test", new Date());
+compareValues("test", 1);
+compareValues("test", "test");
+}
+
+
